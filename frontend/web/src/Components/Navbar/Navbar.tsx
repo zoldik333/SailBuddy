56d633673988b10bc804615fd552af88895c651f
lowan <lowan.ferraro@epitech.eu>
2024-11-18 11:14:39 +0100

    [ADD] navbar style

--- frontend/web/src/Components/Navbar/Navbar.tsx
+++ frontend/web/src/Components/Navbar/Navbar.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import {Link, useLocation} from 'react-router-dom';
 export default function Navbar() {
+  const location = useLocation();
+
+  const isActive = (path: string) => location.pathname === path;
+
   return (
     <div className="w-full bg-[#D0EBF7] h-auto flex items-center justify-between px-16 py-8">
       <Link to={"/"}>
-        <img src="/assets/navbar/logo-sailbuddy.png" alt="" className="h-20 cursor-pointer"/>
+        <img src="/assets/navbar/logo-sailbuddy.png" alt="" className="h-20 cursor-pointer" />
       </Link>
-      <div className={"flex items-center gap-32"}>
-        <Link to={"/historique"}>
-          <div className={"font-bold text-[#1A2B78] text-2xl cursor-pointer"}>Historique</div>
-        </Link>
-        <Link to={"/aide"}>
-          <div className={"font-bold text-[#1A2B78] text-2xl cursor-pointer"}>Aide</div>
-        </Link>
-        <Link to={"/profil"}>
-          <div className={"cursor-pointer"}><img src={"/assets/navbar/profil.png"} alt={"Profil"} className={"h-16"}/></div>
-        </Link>
+
+      <div className="flex items-center gap-32">
+        <div className="flex flex-col items-center relative">
+          <Link to={"/historique"}>
+            <div className="z-20 font-bold text-[#1A2B78] text-2xl cursor-pointer relative">Historique</div>
+          </Link>
+          {isActive('/historique') && (
+            <div className="w-[110%] h-[4px] bg-[#EF4445] absolute bottom-[5px] z-0"></div>
+          )}
+        </div>
+
+        <div className="flex flex-col items-center relative">
+          <Link to={"/aide"}>
+            <div className="z-10 font-bold text-[#1A2B78] text-2xl cursor-pointer relative">Aide</div>
+          </Link>
+          {isActive('/aide') && (
+            <div className="w-[110%] h-1.5 bg-[#EF4445] absolute bottom-[5px] z-0"></div>
+          )}
+        </div>
+
+        <div className="cursor-pointer">
+          <Link to={"/profil"}>
+            <img src="/assets/navbar/profil.png" alt={"Profil"} className="h-16" />
+          </Link>
+        </div>
       </div>
     </div>
-  )
-};
+  );
+}
