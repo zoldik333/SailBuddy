0b95f0b30acc006aa9e5cddd6b1e91fa375dd7fa
lowan <lowan.ferraro@epitech.eu>
2024-11-27 14:27:51 +0100

    [UPD] new navbar style

--- frontend/web/src/Components/Navbar/Navbar.tsx
+++ frontend/web/src/Components/Navbar/Navbar.tsx
@@ -1,41 +1,67 @@
 import React from 'react';
-import {Link, useLocation} from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import "./Navbar.css";
+
 export default function Navbar() {
   const location = useLocation();
 
   const isActive = (path: string) => location.pathname === path;
 
   return (
-    <div className="w-full bg-[#D0EBF7] h-auto flex items-center justify-between px-16 py-8">
-      <Link to={"/"}>
-        <img src="/assets/navbar/logo-sailbuddy.png" alt="" className="h-20 cursor-pointer" />
-      </Link>
+    <div className="fixed flex top-0 left-0 h-full w-[20%] bg-[#FFD3B9] flex-col items-center py-16 shadow-lg">
 
-      <div className="flex items-center gap-32">
-        <div className="flex flex-col items-center relative">
-          <Link to={"/historique"}>
-            <div className="z-20 font-bold text-[#1A2B78] text-2xl cursor-pointer relative">Historique</div>
-          </Link>
-          {isActive('/historique') && (
-            <div className="w-[110%] h-[4px] bg-[#EF4445] absolute bottom-[5px] z-0"></div>
-          )}
-        </div>
-
-        <div className="flex flex-col items-center relative">
-          <Link to={"/aide"}>
-            <div className="z-10 font-bold text-[#1A2B78] text-2xl cursor-pointer relative">Aide</div>
-          </Link>
-          {isActive('/aide') && (
-            <div className="w-[110%] h-[4px] bg-[#EF4445] absolute bottom-[5px] z-0"></div>
-          )}
-        </div>
-
-        <div className="cursor-pointer">
-          <Link to={"/profil"}>
-            <img src="/assets/navbar/profil.png" alt={"Profil"} className="h-16" />
-          </Link>
-        </div>
+      <div className="flex flex-1 flex-col items-center gap-12">
+        <Link to={"/"} className={"relative"}>
+          <div
+            className={`font-bold text-[#1A2B78] text-xl px-4 py-2 rounded-lg relative z-10`}
+          >
+            Accueil
+          </div>
+          <div className={`absolute inset-0 ${isActive('/') ? 'link-active' : ''}`}></div>
+        </Link>
+        <Link to={"/dashboard"}>
+          <div
+            className={`font-bold text-[#1A2B78] text-xl px-4 py-2 rounded-lg ${
+              isActive('/dashboard') ? 'link-active' : ''
+            }`}
+          >
+            Tableau de bord
+          </div>
+        </Link>
+        <Link to={"/history"}>
+          <div
+            className={`font-bold text-[#1A2B78] text-xl px-4 py-2 rounded-lg ${
+              isActive('/history') ? 'link-active' : ''
+            }`}
+          >
+            Historique
+          </div>
+        </Link>
+        <Link to={"/help"}>
+          <div
+            className={`font-bold text-[#1A2B78] text-xl px-4 py-2 rounded-lg ${
+              isActive('/help') ? 'link-active' : ''
+            }`}
+          >
+            Aide
+          </div>
+        </Link>
+        <Link to={"/profile"}>
+          <div
+            className={`font-bold text-[#1A2B78] text-xl px-4 py-2 rounded-lg ${
+              isActive('/profile') ? 'link-active' : ''
+            }`}
+          >
+            Profil
+          </div>
+        </Link>
       </div>
+
+        <img
+          src="/assets/navbar/logo-sailbuddy.png"
+          alt=""
+          className="h-20"
+        />
     </div>
   );
 }
